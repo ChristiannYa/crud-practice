58f385bbdf2a7e3e698887a955485e4dd15027a4
ChristiannYa <christian.ylopez02@gmail.com>
2025-02-01 16:56:26 -0600

    updated lettersOnly middleware

--- server/middleware/lettersOnly.mjs
+++ server/middleware/lettersOnly.mjs
@@ -1,10 +1,23 @@
-export const lettersOnly = (fieldName) => (req, res, next) => {
-  const lettersOnly = /^[A-Za-z\s]+$/;
-  const value = req.body[fieldName];
+export const lettersOnly = (field) => (req, res, next) => {
+  const value = req.body[field];
+  const MIN_LENGTH = 3;
+  const MAX_LENGTH = 50; // Make sure to adjust this value based on the databse
 
-  if (!lettersOnly.test(value)) {
+  if (!value) {
     return res.status(400).json({
-      error: `${fieldName} must contain only letters`,
+      error: `${field} is required`,
+    });
+  }
+
+  if (value.length < MIN_LENGTH || value.length > MAX_LENGTH) {
+    return res.status(400).json({
+      error: `${field} must be between ${MIN_LENGTH} and ${MAX_LENGTH} characters`,
+    });
+  }
+
+  if (!/^[a-zA-Z\s]+$/.test(value)) {
+    return res.status(400).json({
+      error: `${field} must contain only letters and spaces`,
     });
   }
 
